47ab521914fba227898bcfc9ee07c7683e2cb9b6
Denise <yu.denise.d@gmail.com>
2014-09-11 15:02:23 +0100

    Added interactive menu (Exercise 6)

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+
+
 def input_students
 	puts "Enter the names of the students"
 	# create an empty array
@@ -16,10 +18,12 @@ def input_students
 	students
 end
 
+
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "----------------"
 end
+
 
 def print(students)
 	students.to_enum.with_index(1).each do |student, i|
@@ -31,14 +35,36 @@ def print_footer(names)
 	puts "Overall we have #{names.length} great students"
 end
 
+
+def interactive_menu
+	students = []
+	loop do
+	# 1. print the menu and ask the user what to do
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+	# 2. read the input and save it into a variable
+	selection = gets.chomp
+	# 3. do what the user has just asked
+	case selection
+		when "1"
+			students = input_students
+		when "2"
+			print_header
+			print(students)
+			print_footer(students)
+		when "9"
+			exit 
+		else
+			puts "I don't know what you mean, try again"
+		end
+	end
+end
+
+
 #Nothing happens until we call the methods
 
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
-
+interactive_menu
 
 
 
