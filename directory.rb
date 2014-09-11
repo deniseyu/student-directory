ce12d86ab91e0e2e927c85ad966bfea9fb2e2d1b
Denise <yu.denise.d@gmail.com>
2014-09-11 16:11:51 +0100

    Added save students to file

--- directory.rb
+++ directory.rb
@@ -65,6 +65,18 @@ def process(selection)
 end
 
 
+def save_students
+	# open the file for writing
+	file = File.open("students.csv", "w")
+	# iterate over the array of students
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join(", ")
+		file.puts csv_line
+	end
+	file.close
+end
+
 #Nothing happens until we call the methods
 
 interactive_menu
