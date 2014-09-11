0781f71d4c522c79f36211ab5fa358688e616786
Denise <yu.denise.d@gmail.com>
2014-09-11 15:23:42 +0100

    Refactored interactive menu

--- directory.rb
+++ directory.rb
@@ -1,21 +1,18 @@
-
+@students = [] # an empty array accessible to all methods
 
 def input_students
 	puts "Enter the names of the students"
-	# create an empty array
-	students = []
+	puts "To finish, hit enter twice"
 	# gets the first name
 	name = gets.chomp
 	# while the name is not empty, repeat this code
 	while !name.empty? do 
 		# add student hash to the array
-		students << {:name => name, :cohort => :november}
-		puts "Now we have #{students.length} students."
+		@students << {:name => name, :cohort => :november}
+		puts "Now we have #{@students.length} students."
 		# gets another name from the user
 		name = gets.chomp
 	end
-	# return the array of students
-	students
 end
 
 
@@ -25,39 +22,45 @@ def print_header
 end
 
 
-def print(students)
-	students.to_enum.with_index(1).each do |student, i|
-		puts "#{i}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_students_list
+	@students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
-def print_footer(names)
-	puts "Overall we have #{names.length} great students"
+def print_footer
+	puts "Overall we have #{@students.length} great students"
 end
 
-
 def interactive_menu
-	students = []
 	loop do
-	# 1. print the menu and ask the user what to do
+		print_menu
+		process(gets.chomp)
+	end
+end
+
+def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
 	puts "9. Exit"
-	# 2. read the input and save it into a variable
-	selection = gets.chomp
-	# 3. do what the user has just asked
+end 
+
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
+
+def process(selection)
 	case selection
-		when "1"
-			students = input_students
-		when "2"
-			print_header
-			print(students)
-			print_footer(students)
-		when "9"
-			exit 
-		else
-			puts "I don't know what you mean, try again"
-		end
+	when "1"
+		input_students
+	when "2"
+		show_students
+	when "9"
+		exit
+	else
+		puts "I don't know what you mean, try again"
 	end
 end
 
