49280c68fa6c60161e62b3da7de038d8bdc84bf3
Denise <yu.denise.d@gmail.com>
2014-09-11 16:43:08 +0100

    Add load list option

--- directory.rb
+++ directory.rb
@@ -15,6 +15,14 @@ def input_students
 	end
 end
 
+def load_students
+	file = File.open("students.csv", "r")
+	file.readlines.each do |line|
+		name, cohort = line.chomp.split(',')
+		@students << {:name => name, :cohort => cohort.to_sym}
+	end
+	file.close
+end
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -43,6 +51,7 @@ def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
 	puts "3. Save the list"
+	puts "4. Load the list from students.csv"
 	puts "9. Exit"
 end 
 
@@ -60,6 +69,8 @@ def process(selection)
 		show_students
 	when "3"
 		save_students 
+	when "4"
+		load_students
 	when "9"
 		exit
 	else
